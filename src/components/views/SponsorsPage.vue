b1a072885b858fded3138200faebb9734e630836
Your Name <your.email@example.com>
2024-08-30 13:56:32 +1000

    Sponsor page dynamic data

--- src/components/views/SponsorsPage.vue
+++ src/components/views/SponsorsPage.vue
@@ -1,7 +1,21 @@
 <template>
-  <h1>Sponsors</h1>
+  <div class="text-center">
+    <h1>Sponsors</h1>
+    <ul>
+      <li v-for="sponsor in sponsors" :key="sponsor.id">
+        <a :href="sponsor.website" target="_blank">
+          <p>{{ sponsor.name }}</p>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import json from '../Json/Json.json'
+
+const sponsors = ref(json)
+</script>
 
 <style></style>
