0a56ebf798346794726a1e71089779eefd4bfb1a
Your Name <your.email@example.com>
2024-10-09 16:33:39 +1100

    Add function of export as a cvs file in sponsors page

--- src/components/views/SponsorsPage.vue
+++ src/components/views/SponsorsPage.vue
@@ -8,14 +8,33 @@
         </a>
       </li>
     </ul>
+    <button @click="exportCSV">Export as a .csv file</button>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import json from '../Json/Json.json'
+import Papa from 'papaparse'
 
 const sponsors = ref(json)
+
+const exportCSV = () => {
+  const csvData = sponsors.value.map((sponsor) => ({
+    Name: sponsor.name,
+    Website: sponsor.website
+  }))
+
+  const csv = Papa.unparse(csvData)
+
+  const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(blob)
+  link.setAttribute('download', 'sponsors.csv')
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+}
 </script>
 
-<style></style>
+<style scoped></style>
